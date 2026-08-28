c1915dd23da6d8999e9ec324a6058353b99e61c2
Max <harishcruise155@gmail.com>
2026-08-28 14:34:18 +0530

    Initial Commit

---
+++ Stella/StellaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StellaApp.swift
+//  Stella
+//
+//  Created by Harish Maheshwaran on 28/08/26.
+//
+
+import SwiftUI
+
+@main
+struct StellaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
